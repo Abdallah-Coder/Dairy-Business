dba688e948df49947b4ed602dbf956769e8081e2
Abdallah78666 <abdallahabdul056@gmail.com>
2021-03-14 22:47:04 +0300

    add index.html index.css style.css

---
+++ style.js
@@ -0,0 +1,44 @@
+var shedA = shedA(510, 45);
+document.getElementById("shed").innerHTML= "shed A: " + shedA;
+
+function shedA(litres, shillings){
+    return litres*shillings
+}
+    console.log(shed)
+  
+ var shedB = shedB(308, 45);
+ document.getElementById("blue") .innerHTML= "shed B:" + shedB;
+ function shedB(litres, shillings){
+     return litres*shillings
+    
+
+ }
+ console.log(shed)
+
+ var shedC=shedC(486, 45);
+ document.getElementById("pink") .innerHTML= "shed C:" + shedC;
+ function shedC(litres, shillings){
+     return litres*shillings
+ }
+ console.log(shedC)
+
+ var shedD = shedD(572, 45);
+ document.getElementById("grey") .innerHTML= "shed D:" + shedD;
+ function shedD(litres,shillings){
+     return litres*shillings
+ }
+ console.log(shedD)
+
+ var TotalProduce=TotalProduce(1876, 45);
+ document.getElementById("green") .innerHTML= "Total Produce:" + TotalProduce;
+ function TotalProduce(litres, shillings){
+     return litres*shillings
+ }
+ console.log(TotalProduce)
+
+ var TotalAmountinallSheds=TotalAmountinallSheds((510, 45)(308, 45)(486, 45)(572, 45)(486,45));
+ document.getElementById("violet") .innerHTML= "Total Amount in All Sheds:" + TotalAmountinallSheds;
+ function TotalProduce(litres, shillings){
+     return litres*shillings
+ }
+ console.log(TotalAmountinallSheds)
