e0796457e9ccc79b22984602160fbc8fe534a60d
Abdallah78666 <abdallahabdul056@gmail.com>
2021-03-15 22:51:12 +0300

    modify index.html index.css LICENSE README.md style.js

---
+++ README.md
@@ -0,0 +1,13 @@
+# Dairy-Business
+## Author
+Abdallah Habib
+### Description
+This is a website that contains the output of the amounth that a dealer of milk tender gets.
+
+### Technology Used
+HTML
+CSS
+JavaScript
+Git
+### Live site
+view (live)https://abdallah78666.github.io/Dairy-Business/
--- style.js
+++ style.js
@@ -39,9 +39,128 @@ function shedA(litres, shillings){
  
 
 
- var weeklyincome=weeklyincome(1876, 45);
+ var weeklyincome=weeklyincome(590940, 7);
  document.getElementById("one") .innerHTML= "Weekly Income:" + weeklyincome;
  function weeklyincome(litres, shillings){
      return litres*shillings
  }
 console.log(weeklyincome)
+
+
+var YearlyIncome=YearlyIncome(30813300, 365);
+document.getElementById("two") .innerHTML= "Yearly Income:" + YearlyIncome;
+function YearlyIncome(litres, shillings){
+    return litres*shillings
+}
+console.log(YearlyIncome)
+
+
+var January=January(2617020,31);
+document.getElementById("jan") .innerHTML= "January:" + January;
+function January(litres, shillings){
+    return litres*shillings
+}
+console.log(January)
+
+
+var February=February(2448180, 28);
+document.getElementById("feb") .innerHTML= "February:" + February;
+function February(litres, shillings){
+    return litres*shillings
+}
+console.log(February)
+
+
+var March=March(2617020,30);
+document.getElementById("march") .innerHTML= "march:" + March;
+function March(litres, shillings){
+    return litres*shillings
+}
+console.log(March)
+
+
+var April=April(2532600,31);
+document.getElementById("april") .innerHTML= "April:" + April;
+function April(litres, shillings){
+    return litres*shillings
+}
+console.log(April)
+
+
+var May=May(2617020,30);
+document.getElementById("may") .innerHTML= "May:" + May;
+function May(litres, shillings){
+    return litres*shillings
+}
+console.log(May)
+
+
+
+var June=June(2532600,31);
+document.getElementById("june") .innerHTML= "June:" + June;
+function June(litres, shillings){
+    return litres*shillings
+}
+console.log(June)
+
+
+var July=July(2617020,30);
+document.getElementById("july") .innerHTML= "July:" + July;
+function July(litres, shillings){
+    return litres*shillings
+}
+console.log(July)
+
+
+
+var August=August(2617020,31);
+document.getElementById("august") .innerHTML= "August:" + August;
+function August(litres, shillings){
+    return litres*shillings
+}
+console.log(August)
+
+
+var September=September(2532600,30);
+document.getElementById("sept") .innerHTML= "September:" + September;
+function September(litres, shillings){
+    return litres*shillings
+}
+console.log(September)
+
+
+var October=October(2617020,31);
+document.getElementById("october") .innerHTML= "October:" + October;
+function October(litres, shillings){
+    return litres*shillings
+}
+console.log(October)
+
+
+var November=November(2532600,30);
+document.getElementById("november") .innerHTML= "November:" + November;
+function November(litres, shillings){
+    return litres*shillings
+}
+console.log(November)
+
+
+
+
+
+
+var December=December(2617020,31);
+document.getElementById("december") .innerHTML= "December:" + December;
+function December(litres, shillings){
+    return litres*shillings
+}
+console.log(December)
+
+
+
+
+
+
+
+
+
